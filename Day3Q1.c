f7d9f34d5c0e1e842c638864a210050ce36a430c
Anidhya Verma <255174827+anidhyavercon-cyber@users.noreply.github.com>
2026-02-07 16:01:52 +0530

    Update Day3Q1.c

--- Day3Q1.c
+++ Day3Q1.c
@@ -1,39 +1,34 @@
 #include <stdio.h>
-int main () {
-    int n;
-    int e;
-    int i;
-    printf("enter size of the array : ");
-    scanf("%d", &n);
 
-    int arr[n];
-    
-    printf("enter %d elements : ", n);
-    for(int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
-    }
+int main() {
 
-    printf("array : ");
-    for(int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-     
-printf("enter element to search : ");
-scanf("%d", &e);
-        i = 0;
-     while (i < n) {
-       if (arr[i] == e ) {
-            printf("%d\n", e);
-            printf("found at %d ", i);
-            printf("comparison count = %d", i);
+int n;
+printf("enter the size of array: ");
+scanf("%d" , &n);
 
-     }
-        i++;
-    }
-    
-
-
-
-    return 0;
+int arr[100];
+printf("enter %d integers\n" , n);
+for (int i=0 ; i<n ; i++) {
+scanf("%d" , &arr[i]); 
+};
+printf("array: ");
+for (int i=0 ; i<n ; i++) {
+    printf("%d " , arr[i]);
 }
+
+int e;
+int i = 0;
+printf("\nenter the number to search: ");
+scanf("%d" , &e);
+while (i<n) {
+    if (arr[i] == e) {
+        printf("Element found at %d\n" , i);
+        printf("Comparision count: %d", i);
+    }
+    i++;
+}
+
+return 0;
+
+}
+
